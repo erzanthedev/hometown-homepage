422477fd1aa1cdb5e76df86e304ac573242d5e3e
Michael Erzan Andoh <erzanthedev@gmail.com>
2025-02-12 21:06:43 +1100

    built the hero section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Hero from "./components/Hero";
+
 function App() {
-  return <h1>Hometown Homepage</h1>;
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <div id="hero">
+      <h1 id="hero-title">Visit Torshov</h1>
+      <h2 id="hero-text">
+        Enjoy culture and sports in this vibrant district close to the center of
+        Oslo.
+      </h2>
+    </div>
+  );
+};
+
+export default Hero;
